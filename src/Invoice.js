859d33b56812bcd9d6ef1ecb774938e79db35d9d
yummydao <rogerscott480@gmail.com>
2024-09-09 10:48:44 +0100

    cha

--- src/Invoice.js
+++ src/Invoice.js
@@ -330,7 +330,7 @@ export default function PermanentDrawerLeft() {
                   </div>
                   <div className='justcenter flex aligncenter column width20 mb2'>
                       {/*<Typography>Deposits</Typography>*/}
-                      <Typography>{new Date(user?.date ).toLocaleString()}</Typography>
+                      <Typography>{new Date(user?.date )}</Typography>
                   </div>
                   <div className='justcenter flex aligncenter column width20 mb2'>
                       {/*<Typography>Deposits</Typography>*/}
