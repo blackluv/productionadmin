f975e2da85dacc0eb5028a7579d3f0c5c5d142b9
yummydao <rogerscott480@gmail.com>
2024-09-09 08:48:08 +0100

    fjj

--- src/Transactions.js
+++ src/Transactions.js
@@ -443,7 +443,7 @@ export default function PermanentDrawerLeft() {
                 </Card>
                 <Typography className='mb2'>Merchant Wallet Transactions</Typography>
               <div className='spacearound flex mt2 bottom'>
-                  {rest ? rest?.map((rest) => (
+                  {/*rest ? rest?.map((rest) => (
                     <Card className='width mb2'>
                       <CardContent className='spacebetween flex'>
                         <div className='justcenter flex aligncenter column'>
@@ -464,7 +464,7 @@ export default function PermanentDrawerLeft() {
                         </div>
                       </CardContent>
                     </Card>
-                    )) : <Typography></Typography>}
+                    )) : <Typography></Typography>*/}
                   {rest1 ? rest1?.map((rest1) => (
                     <Card className='width mb2'>
                       <CardContent className='spacebetween flex'>
