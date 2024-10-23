f9f3d28bef1e6c1141aecf8010b7b88ddf78a3bb
yummydao <rogerscott480@gmail.com>
2024-10-23 15:58:32 +0100

    sjj

--- src/User.js
+++ src/User.js
@@ -162,7 +162,7 @@ async function enableuser(shop) {
       body: urlencoded
     });
 
-    const data = await response.json();
+    const data = await response;
     console.log(data, 'data')
 
     // Alert the response data
@@ -186,7 +186,7 @@ async function disableuser(shop) {
       body: urlencoded
     });
 
-    const data = await response.json();
+    const data = await response;
     console.log(data, 'data')
 
     // Alert the response data
